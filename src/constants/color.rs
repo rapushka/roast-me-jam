59c16e3e712f584f56c87bdeeee0f1af424bbd96
Rapuska <mikalajsiatkouski@gmail.com>
2024-06-02 21:03:45 +0300

    new colors

--- src/constants/color.rs
+++ src/constants/color.rs
@@ -1,10 +1,10 @@
 use bevy::prelude::*;
 
-pub const DEFAULT_BUTTON: Color = Color::rgb(0.93, 0.71, 0.32);
-pub const HOVERED_BUTTON: Color = Color::rgb(0.9, 0.81, 0.54);
-pub const PRESSED_BUTTON: Color = Color::rgb(0.89, 0.58, 0.28);
+pub const DEFAULT_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+pub const HOVERED_BUTTON: Color = Color::rgb(0.7, 0.7, 0.7);
+pub const PRESSED_BUTTON: Color = Color::rgb(0.58, 0.58, 0.58);
 
-pub const DEFAULT_TEXT: Color = Color::rgb(0.30, 0.25, 0.22);
+pub const DEFAULT_TEXT: Color = Color::rgb(0.25, 0.25, 0.25);
 pub const LIGHT_TEXT: Color = Color::rgb(0.9, 0.9, 0.9);
 
 pub const LIGHT_GREEN_TEXT: Color = Color::rgb(0.1, 1.0, 0.1);
