57de490382f4994a6a628961bf110c999f47d6ec
Rapuska <mikalajsiatkouski@gmail.com>
2024-06-02 17:03:40 +0300

    paint

--- src/gameplay/plants/plant_preview.rs
+++ src/gameplay/plants/plant_preview.rs
@@ -1,10 +1,14 @@
+use bevy::math::bounding::Aabb2d;
 use bevy::prelude::*;
 use crate::controls::Input;
+use crate::gameplay::field::Field;
 use crate::gameplay::seeds::initialization::get_sprite;
 use crate::gameplay::seeds::planting::{PlantingState, SelectedSeed};
 
-#[derive(Component)]
-pub struct PlantPreview;
+#[derive(Component, Default)]
+pub struct PlantPreview {
+    pub can_plant: bool,
+}
 
 pub struct PlantPreviewPlugin;
 
@@ -12,6 +16,11 @@ impl Plugin for PlantPreviewPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(OnEnter(PlantingState::Planting), create_plant_preview)
+
+            .add_systems(Update, (
+                paint_plant_preview
+            ).run_if(in_state(PlantingState::Planting)))
+
             .add_systems(OnExit(PlantingState::Planting), destroy_plant_preview)
         ;
     }
@@ -31,13 +40,12 @@ fn create_plant_preview(
     let plant_type = selected_seed.0.unwrap();
 
     commands.spawn(Name::new("plnat preview"))
-        .insert(PlantPreview)
+        .insert(PlantPreview::default())
         .insert(SpriteBundle {
             texture: asset_server.load(get_sprite(plant_type)),
             transform: Transform::from_translation(spawn_point),
             ..default()
         })
-    // .insert(Color::RED)
     ;
 }
 
@@ -48,4 +56,27 @@ fn destroy_plant_preview(
     for e in previews.iter() {
         commands.entity(e).despawn_recursive();
     }
+}
+
+fn paint_plant_preview(
+    mut previews: Query<&mut Sprite, With<PlantPreview>>,
+    field: Res<Field>,
+    input: Res<Input>,
+) {
+    for mut sprite in previews.iter_mut() {
+        let cursor_position = input.mouse_world_position.unwrap_or_default();
+        let bounds = field.plants_bounds;
+
+        let valid_position = contains(bounds, cursor_position);
+
+        sprite.color = if valid_position {
+            Color::Rgba { red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5 }.into()
+        } else {
+            Color::Rgba { red: 1.0, green: 0.0, blue: 0.0, alpha: 0.5 }.into()
+        };
+    }
+}
+
+fn contains(aabb: Aabb2d, point: Vec2) -> bool {
+    point.x >= aabb.min.x && point.x <= aabb.max.x && point.y >= aabb.min.y && point.y <= aabb.max.y
 }
